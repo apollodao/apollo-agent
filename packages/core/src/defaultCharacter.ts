f640b21bb74b583e97090211bf2d0cc282cbcf18
J. Brandon Johnson <bjohnson@swizzmagik.com>
2025-01-10 09:27:53 -0800

    chore: fix failing ci

--- packages/core/src/defaultCharacter.ts
+++ packages/core/src/defaultCharacter.ts
@@ -1,10 +1,10 @@
-import { Character, Clients, ModelProviderName } from "./types.ts";
+import { Character, ModelProviderName } from "./types.ts";
 
 export const defaultCharacter: Character = {
     name: "Eliza",
     username: "eliza",
     plugins: [],
-    clients: [Clients.DISCORD],
+    clients: [],
     modelProvider: ModelProviderName.ANTHROPIC,
     settings: {
         secrets: {},
